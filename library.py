a33c6dd838d3ee42cbf59317d355177e85b57542
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2023-12-29 23:06:28 +0000

    Started converting my student database to a home library. Still much work to do.

--- library.py
+++ library.py
@@ -1,12 +1,12 @@
 import json
 import os
 
-# Function to save the student dictionary to a JSON file
+# Function to save the home library to a JSON file
 def save_json(home_library):
     with open(LIBRARY_RECORD, "w") as file:
         json.dump(home_library, file, indent=4)
 
-# Function to load the student dictionary from a JSON file
+# Function to load the home library from a JSON file
 def open_json():
     if os.path.exists(LIBRARY_RECORD):
         with open(LIBRARY_RECORD, "r") as file:
@@ -17,99 +17,102 @@ def open_json():
     else:
         return {}
 
-# Define a filename for the student record JSON file
+# Define a filename for the home library JSON file
 LIBRARY_RECORD = "/Users/garybutterfield/GitHub/home-library/home_library.json"
 
-# Load the existing student dictionary or create an empty one
+# Load the existing home library or create an empty one
 home_library = open_json()
 
 while True:
     print("\nWelcome to your home library.\n")
-    print("1. Add a Book\n2. Search for a Book\n3. List All Books\n4. Update a Library Record\n5. Delete Book\n6. Exit\n")
+    print("1. Add a library record\n2. Search for a library record\n3. List all library records\n4. Update a library record\n5. Delete library record\n6. Exit\n")
 
     choice = input("Please select an option: ")
 
-    if choice == "1": # Add a student record
+    if choice == "1": # Add a library record
         print("\nYou chose to add a new book\n")
 
-        student_id = input("Enter Student ID: ")
-        name = input("Enter Student Name: ")
-        age = input("Enter Student Age: ")
-        subject = input("Enter Student Subject: ")
+        book_title = input("Enter the book titleD: ")
+        author = input("Enter the author: ")
+        rating = input("Enter your rating out of 5*: ")
+        review = input("Enter your review: ")
 
-        student = {
-            "ID": student_id,
-            "NAME": name,
-            "AGE": age,
-            "SUBJECT": subject
+        book = {
+            "TITLE": book_title,
+            "AUTHOR": author,
+            "RATING": rating,
+            "REVIEW": review
         }
 
-        home_library[student_id] = student
+        home_library[book_title] = book
         save_json(home_library)
 
         print("Student record added successfully.")
 
-    elif choice == "2": # Search for a student record
-        print("\nYou chose to search for a book\n")
+    elif choice == "2": # Search for a library record
+        print("\nYou chose to search for a library record\n")
 
-        student_id = input("Enter Student ID to search: ")
+        book_title = input("Enter Student ID to search: ")
 
-        if student_id in home_library:
-            student = home_library[student_id]
-            print("ID:", student["ID"])
-            print("Name:", student["NAME"])
-            print("Age:", student["AGE"])
-            print("Subject:", student["SUBJECT"])
+        if book_title in home_library:
+            student = home_library[book_title]
+            print("Title:", book_title["TITLE"])
+            print("Author:", author["AUTHOR"])
+            print("Rating:", rating["RATING"])
+            print("Review:", review["REVIEW"])
         else:
-            print("Student record not found.")
+            print("Library record not found.")
 
-    elif choice == "3": # List all records
+    elif choice == "3": # List all library records
         print("\nYou chose to list all library books\n")
 
-        for student_id, student in home_library.items():
-            print(f"ID: {student_id}, Name: {student['NAME']}, Age: {student['AGE']}, Subject: {student['SUBJECT']}")
+        for book_title, book in home_library.items():
+            print(f"Title: {book_title}, Author: {author['AUTHOR']}, Rating: {rating['RATING']}, Review: {review['REVIEW']}")
 
-    elif choice == "4": # Update student record
+    elif choice == "4": # Update library record
         print("\nYou chose to update a library record\n")
 
-        student_id = input("Enter Student ID to update: ")
+        book_title = input("Enter Student ID to update: ")
 
-        if student_id in home_library:
-            student = home_library[student_id]
+        if book_title in home_library:
+            student = home_library[book_title]
 
             print("What would you like to update?")
-            print("1. Name\n2. Age\n3. Subject")
+            print("1. Book Title\n2. Author\n3. Rating\n4. Review")
             update_choice = input("Enter your choice: ")
 
             if update_choice == "1":
-                new_name = input("Enter the new name: ")
-                student["NAME"] = new_name
+                new_author_title = input("Enter your new book title: ")
+                student["TITLE"] = new_author_title
             elif update_choice == "2":
-                new_age = input("Enter the new age: ")
-                student["AGE"] = new_age
+                new_age = input("Enter the new author: ")
+                student["AUTHOR"] = new_age
             elif update_choice == "3":
-                new_subject = input("Enter the new subject: ")
-                student["SUBJECT"] = new_subject
+                new_rating = input("Enter your new rating out of 5*: ")
+                student["RATING"] = new_rating
+            elif update_choice == "4":
+                new_review = input("Enter your new review: ")
+                student["REVIEW"] = new_review
             else:
                 print("Invalid choice.")
 
-            home_library[student_id] = student
+            home_library[book_title] = book
             save_json(home_library)
-            print("Student record updated successfully.")
+            print("Library record updated successfully.")
         else:
             print("Student ID not found. Cannot update the record.")
 
-    elif choice == "5": # Delete student record
+    elif choice == "5": # Delete library record
         print("\nYou chose to delete a library record\n")
 
-        student_id_to_delete = input("Enter Student ID to delete: ")
+        library_record_to_delete = input("Enter the book title to delete: ")
 
-        if student_id_to_delete in home_library:
-            del home_library[student_id_to_delete]
+        if library_record_to_delete in home_library:
+            del home_library[library_record_to_delete]
             save_json(home_library)
-            print(f"Student record with ID {student_id_to_delete} has been deleted.")
+            print(f"Library record with the {library_record_to_delete} book title has been deleted.")
         else:
-            print("Student ID not found. Cannot delete the record.")
+            print("Library record not found. Cannot delete the record.")
 
     elif choice == "6": # Exit
         print("\nExiting the program.")
